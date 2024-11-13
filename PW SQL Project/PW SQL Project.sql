0432415b858e4438249f12c6a176842b13283174
Jatin Kumar Varshney <117568544+JatinKrVarshney@users.noreply.github.com>
2024-11-13 11:30:43 +0530

    Update PW SQL Project.sql

--- PW SQL Project/PW SQL Project.sql
+++ PW SQL Project/PW SQL Project.sql
@@ -1,11 +1,11 @@
 1. Create a table called employees with the following structure?
-- emp_id (integer, should not be NULL and should be a primary key)Q
-- emp_name (text, should not be NULL)Q
-- age (integer, should have a check constraint to ensure the age is at least 18)Q
-- email (text, should be unique for each employee)Q
+- emp_id (integer, should not be NULL and should be a primary key).
+- emp_name (text, should not be NULL).
+- age (integer, should have a check constraint to ensure the age is at least 18).
+- email (text, should be unique for each employee).
 - salary (decimal, with a default value of 30,000).
 Write the SQL query to create the above table with all constraints.
-USE pw
+
 CREATE TABLE employees (
     emp_id INT NOT NULL PRIMARY KEY,
     emp_name VARCHAR(255) NOT NULL,
